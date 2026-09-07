d160b49d1f2c79cdfc8b9c9fe8023021dd1441cd
mustafasglm2012-creator <mustafasglm2012@gmail.com>
2026-09-07 09:44:45 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'myurgentbox-v1';
+const ASSETS = [
+  '/myurgentbox/',
+  '/myurgentbox/index.html',
+  '/myurgentbox/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
